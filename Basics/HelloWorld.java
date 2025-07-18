7b87387e799808229cbdd3a61eaa6756d6957d30
Soham Bhadra <soham.bhdr@gmail.com>
2025-07-19 02:36:48 +0530

    Deleted files

--- Basics/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Variable declaration
-        int number = 1729;  // Integer data type
-        String message = "Hello, Java!";  // String (reference type)
-        
-        System.out.println(message + " Your number is: " + number);
-    }
-}
